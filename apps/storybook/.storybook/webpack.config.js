d06bed2d94dad55a03407d3dcbaed2819e21b47b
Filip Messa <messa.filip@gmail.com>
2020-03-05 12:19:10 +0100

    feat(storybook) add support for the react native web

--- apps/storybook/.storybook/webpack.config.js
+++ apps/storybook/.storybook/webpack.config.js
@@ -1,10 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ config }) => {
-    config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve('awesome-typescript-loader')
-    });
-    config.resolve.extensions.push('.ts','.tsx');
-    return config;
-}
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve("awesome-typescript-loader")
+  });
+  config.resolve.extensions.push(".ts", ".tsx");
+  config.resolve.alias = {
+    "react-native$": "react-native-web"
+  };
+  return config;
+};
--- apps/storybook/stories/0-Welcome.stories.tsx
+++ apps/storybook/stories/0-Welcome.stories.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
+import { View } from  'react-native';
 
 export default {
   title: 'Welcome',
   component: Welcome,
 };
 
-export const ToStorybook = () => <Welcome showApp={linkTo('Button')} />;
+export const ToStorybook = () => <View><Welcome showApp={linkTo('Button')} /></View>;
 
 ToStorybook.story = {
   name: 'to Storybook',
