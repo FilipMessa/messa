398c18c3af54c0137dc669f6c74ee7c439469874
Filip Messa <messa.filip@gmail.com>
2020-11-01 16:35:19 +0100

    feat(messa-ui): Add color Text story

--- packages/messa-ui/src/components/Text/Text.stories.tsx
+++ packages/messa-ui/src/components/Text/Text.stories.tsx
@@ -8,8 +8,9 @@ export default { title: 'Text' };
 export const basic = () => {
   const as = select('as', ['p', 'span', 'div'], 'span');
   const fontSize = select('fontSize', [0, 1, 2, 3, 4, 5, 6, 7], 1);
+  const color = select('color', ['primary', 'secondary', 'default'], 'default');
   return (
-    <Text fontSize={fontSize} as={as}>
+    <Text fontSize={fontSize} as={as} color={color}>
       {text('children', 'Lorem Ipsum')}
     </Text>
   );
